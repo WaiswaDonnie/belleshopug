0623e308d10fa3cc8169ab01affbeeb300deecad
WaiswaDonnie <waiswadonnie@gmail.com>
2022-09-04 16:06:48 +0300

    made changes on logo

--- shop/src/components/ui/logo.tsx
+++ shop/src/components/ui/logo.tsx
@@ -13,11 +13,14 @@ const Logo: React.FC<React.AnchorHTMLAttributes<{}>> = ({
   } : any = useSettings();
   return (
     <Link href="/makeup" className={cn('inline-flex', className)} {...props}>
-      <span className="relative h-10 w-32 overflow-hidden md:w-40">
+      <span className="relative h-12  w-32 overflow-hidden md:w-40">
         <Image
+        
+        width={170}
+        height={60}
          src={require('../../assets/bellelogo.png')}
           alt={'Belle Shop Logo'}
-          layout="fill"
+          // layout="fixed"
           objectFit="contain"
           loading="eager"
         />
